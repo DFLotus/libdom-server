2a3beebf54edfbf501d0c3162d1305e629087ff7
DFLotus <deondraefrancis101@gmail.com>
2024-05-26 11:42:20 -0400

    Moved application file outside of folder

--- libdom-server/src/main/java/com/example/DummyTest.java
+++ libdom-server/src/main/java/com/example/DummyTest.java
@@ -1,4 +1,4 @@
-package com.example.libdom_server;
+package com.example;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
