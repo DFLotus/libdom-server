fb054cc4c6d2fb87d7e222bdd7bb82a08cda6b1f
DFLotus <deondraefrancis101@gmail.com>
2024-05-28 10:27:59 -0400

    Connected to MongoDB

--- libdom-server/src/main/java/com/server/DummyTest.java
+++ libdom-server/src/main/java/com/server/DummyTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.server;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
--- libdom-server/src/main/java/com/server/LibdomServerApplication.java
+++ libdom-server/src/main/java/com/server/LibdomServerApplication.java
@@ -1,12 +1,15 @@
-package com.example;
+package com.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class LibdomServerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(LibdomServerApplication.class, args);
 	}
 
